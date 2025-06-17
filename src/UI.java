645ec8b0009e576da78da1ce0c84e0b18372fd52
Andres Farfan <a22310560@ceti.mx>
2025-06-17 12:58:23 -0600

    Mejorado resaltado en color de trigonometría

--- src/UI.java
+++ src/UI.java
@@ -24,13 +24,13 @@ public class UI extends Application {
 
     private static final String[] KEYWORDS = {
       "dado", "equivale", "si", "si no", "pero si", "f", "mientras", "desde", "con", "avanzar", "tal que",
-      "conjunto", "PI", "e", "imprimir"
+      "conjunto", "PI", "e", "imprimir", "sen", "cos", "tan", "sec", "csc", "cot"
     };
 
     private static final String KEYWORD_PATTERN = "\\b(" + String.join("|", KEYWORDS).replace(" ", "\\s") + ")\\b";
     private static final String NUMBER_PATTERN = "\\b\\d+(\\.\\d+)?\\b";
     private static final String IMAGINARY_PATTERN = "\\b-?\\d+(\\.\\d+)?i\\b";
-    private static final String DEGREES_PATTERN = "(?<!\\S)-?\\d+(\\.\\d+)?°(?!\\S)";
+    private static final String DEGREES_PATTERN = "(?<!\\w)-?\\d+(\\.\\d+)?°(?!\\w)";
     private static final String OPERATOR_PATTERN = "[+\\-*/=<>!&|%^~]+";
     private static final String BRACE_PATTERN = "[\\[\\]{}()]";
     private static final String COMMENT_PATTERN = "//.*";
